58fd7e3f8d5786aa1a9717f3800c6b7b233dc9c3
dvddvd300 <daviddeleon2001@gmail.com>
2023-05-27 13:48:18 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,59 @@
 module.exports = {
-    content: ['./app/**/*.tsx', './app/**/*.ts'],
-    theme: {
-      colors: {
-        rojobanorte: '#E30029',
-        grisbanorte: '#5B6670',
-        blancobanorte: '#FFFFFF',
-        banorteenlaces: '#EB0029',
-        banortepositivo: '#6CC04A',
-        banortealertas: '#FF671B',
-        banorteavisos: '#FFA400',
-      },
-      fontfamily: {
-        'gotham': ['Gotham', 'sans-serif'],
-        'roboto': ['Roboto', 'sans-serif'],
-      },
-      extend: {
-        
-        aspectRatio: {
-          '4/1': '4 / 1',
-        },
-        
+  content: ['./app/**/*.tsx', './app/**/*.ts'],
+  theme: {
+    colors: {
+      rojobanorte: '#E30029',
+      grisbanorte: '#5B6670',
+      blancobanorte: '#FFFFFF',
+      banorteenlaces: '#EB0029',
+      banortepositivo: '#6CC04A',
+      banortealertas: '#FF671B',
+      banorteavisos: '#FFA400',
+    },
+    fontFamily: {
+      gotham: ['Gotham', 'sans-serif'],
+      roboto: ['Roboto', 'sans-serif'],
+    },
+    extend: {
+      aspectRatio: {
+        '4/1': '4 / 1',
       },
     },
-    plugins: [
-      require('daisyui'),
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter',
     ],
-    daisyui: {
-      themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
-    },
-  };
+  },
+};
