9b5420b9ae9683f20a0c6b99a2aa11232d959434
Rob <106797059+rpribau@users.noreply.github.com>
2023-05-27 13:45:40 -0600

    Hero created.

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -10,15 +10,30 @@ export function Header({ t }: any) {
    
 
   return (
-    <header className="sticky top-0 z-30 flex h-16 w-full justify-center backdrop-blur transition-all duration-100 text-base-content">
+    <header 
+      className="sticky top-0 z-30 flex h-16 w-full justify-center backdrop-blur transition-all duration-100 text-base-content"
+      
+      >
       <nav className="navbar sticky mb-2 shadow-lg bg-neutral text-neutral-content rounded-box col-span-1 xl:col-span-3">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" 
+                   className="h-5 w-5" 
+                   fill="none" 
+                   viewBox="0 0 24 24" 
+                   stroke="currentColor"><path 
+                   strokeLinecap="round" 
+                   strokeLinejoin="round" 
+                   strokeWidth="2" 
+                   d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
-            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-              <li><Link to="/">
+
+            <ul 
+              tabIndex={0} 
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+              
+            <li><Link to="/">
               {t("Home")}
               </Link></li>
               {/* <li><Link to="/d/e?n=-tec-">
