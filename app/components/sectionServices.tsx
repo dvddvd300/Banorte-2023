fe16db27cfbd46da42cfe2a3f0057e9bbf5884b7
Oskar Abraham Sandoval Garcia <osabga@gmail.com>
2023-05-28 10:50:24 -0600

    Updated Lenguage

--- app/components/sectionServices.tsx
+++ app/components/sectionServices.tsx
@@ -40,15 +40,31 @@ export default function Example() {
             role="list"
             className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3"
           >
-            {people.map((person) => (
-              <li key={person.name}>
-                <a href={person.linkUrl}>
-                  <img className="aspect-[3/2] w-full rounded-2xl object-cover" src={person.imageUrl} alt={person.name} />
+
+            <li key={t("Tarjetas de Crédito")}>
+              <a href={"/tarjetascredito"}>
+                <img className="aspect-[3/2] w-full rounded-2xl object-cover" src={'/static/tarjetas-banorte.png'} alt={"Tarjeta de Credito"} />
                 </a>
-                <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">{person.name}</h3>
-                <p className="text-base leading-7 text-gray-600">{person.role}</p>
+              <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">{t('Tarjetas de Crédito')}</h3>
+              <p className="text-base leading-7 text-gray-600">{t('Encuentra la mejor tarjeta de crédito para ti, conoce sus beneficios y comienza a disfrutar de sus ventajas.')  }</p>
               </li>
-            ))}
+            <li key={'Cuenta enlace digital'}>
+              <a href={"/enlacedigital"}>
+                <img className="aspect-[3/2] w-full rounded-2xl object-cover" src={'/static/cuenta-digital-banorte.png'} alt={"Tarjeta de Credito"} />
+              </a>
+              <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">{t('Cuenta enlace digital')}</h3>
+              <p className="text-base leading-7 text-gray-600">{t('Conoce la nueva cuenta enlace digital, la cuenta que te permite realizar tus operaciones bancarias desde tu celular.')}</p>
+            </li>
+            <li key={'Banca Preferente'}>
+              <a href={"/preferente"}>
+                <img className="aspect-[3/2] w-full rounded-2xl object-cover" src={'/static/banorte-web.jpg'} alt={"Tarjeta de Credito"} />
+              </a>
+              <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">{t('Banca Preferente')}</h3>
+              <p className="text-base leading-7 text-gray-600">{t('Conoce la nueva cuenta enlace digital, la cuenta que te permite realizar tus operaciones bancarias desde tu celular.')}</p>
+            </li>
+
+
+
           </ul>
         </div>
       </div>
